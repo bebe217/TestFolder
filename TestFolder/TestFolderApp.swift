add062b602a335b9dfdb8e68a48a80f49eb73701
bebeda <bebe217@naver.com>
2025-05-01 15:35:37 +0900

    Initial Commit

---
+++ TestFolder/TestFolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFolderApp.swift
+//  TestFolder
+//
+//  Created by bebe on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TestFolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
